16703c831881fc406163ba14ffa8f209035b68b5
Jusales <jusales3270@gmail.com>
2026-01-20 08:10:08 -0300

    Fix: Enable DB SSL

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,14 @@ const PORT = 3001;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  console.error("Global Error Handler:", err);
+  res.status(status).json({ message });
+});
+
 app.use((req: Request, res: Response, next: NextFunction) => {
   const message = `[${new Date().toISOString()}] ${req.method} ${req.url}`;
   console.log(message);
